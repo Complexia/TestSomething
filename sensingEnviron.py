f83ba12ac4c86b3ba3e576da293e03405bb0c508
Remi <s3399752@student.rmit.edu.au>
2020-03-24 17:07:37 +1100

    sense display environ

--- sensingEnviron.py
+++ sensingEnviron.py
@@ -8,3 +8,10 @@ humidity = sense.get_humidity()
 print(pressure)
 print(temperature)
 print(humidity)
+
+pressure.round(pressure,1)
+temperature.round(temperature,1)
+humidity.round(humidity,1)
+
+message = (" Pressure: " + str(pressure) + " Temprerature: " + str(temperature) + " Humidity: " + str(humidity))
+sense.show_message(message, scroll_speed=0.05)
