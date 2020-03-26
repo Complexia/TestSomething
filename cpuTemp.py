242ed469ed26c914c3349e1d938f6eb44e212d88
Remi <s3399752@student.rmit.edu.au>
2020-03-26 11:52:49 +1100

    calibratedTemp

--- cpuTemp.py
+++ cpuTemp.py
@@ -1,3 +1,14 @@
 import subprocess
+from sense_hat import SenseHat 
+sense = SenseHat()
 cpuTemp = subprocess.check_output("vcgencmd measure_temp", shell=True)
 print(cpuTemp)
+array1 = cpuTemp.split("=")
+array2 = array1[1].split("'")
+cpuTempFloat = float(array2[0])
+
+temp = sense.getTemperature()
+
+actualTemp = temp - ((cpuTempFloat - temp)/5.5)
+print(actualTemp)
+
