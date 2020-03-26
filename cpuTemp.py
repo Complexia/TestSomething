edaea9ed9a6e85d5321cb5e380bf4bd74b93d82f
Remi <s3399752@student.rmit.edu.au>
2020-03-26 12:09:08 +1100

    quickfix factor

--- cpuTemp.py
+++ cpuTemp.py
@@ -10,6 +10,6 @@ cpuTempFloat = float(array2[0])
 
 temp = sense.get_temperature()
 
-actualTemp = temp - ((cpuTempFloat - temp)/5.5)
+actualTemp = temp - ((cpuTempFloat - temp)/7.5)
 print(actualTemp)
 
