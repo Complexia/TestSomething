a1119368a83a51de9a8e0ff43c667a33c24dc809
Remi <s3399752@student.rmit.edu.au>
2020-03-26 12:04:01 +1100

    qyuckfix

--- cpuTemp.py
+++ cpuTemp.py
@@ -3,7 +3,8 @@ from sense_hat import SenseHat
 sense = SenseHat()
 cpuTemp = subprocess.check_output("vcgencmd measure_temp", shell=True)
 print(cpuTemp)
-array1 = cpuTemp.split("=")
+array1 = str(cpuTemp).split("=")
+
 array2 = array1[1].split("'")
 cpuTempFloat = float(array2[0])
 
